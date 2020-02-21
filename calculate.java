b13ec327ca5c69007033913e4a22385c0e8232b2
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2020-02-21 20:09:55 +0500

    Add files via upload

---
+++ calculate.java
@@ -0,0 +1,13 @@
+public class calculate
+{
+  public static void main (String args[])
+  {
+ 
+float percent=15000;
+
+  
+    System.out.printf("the salary in 8 percent is %f\n",percent * 0.08);
+  System.out.printf("the salary in 5 percent is %f\n",percent * 0.05);
+  System.out.printf("the salary in 10 percent is %f\n",percent * 0.010);
+  }
+}
---
+++ task4.java
@@ -0,0 +1,17 @@
+
+public class calculate{
+
+
+     public static void main(String []args)
+{
+         
+         float price=500.0f;
+     
+    
+         
+         
+        System.out.printf("price reduced by 20% is %f",price*0.020);
+      
+  System.out.printf("new amount will be %f",price-(price*0.020));
+     }
+}
