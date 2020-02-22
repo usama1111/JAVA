3b904eeb44f3cb15e99d744444d3859b092eb1fa
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2020-02-22 15:08:11 +0500

    input from user

---
+++ string.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class room{
+public static void main(String[] args)
+{
+String name;
+Scanner input=new Scanner(System.in);
+System.out.println("enter username");
+name=input.nextLine();
+if(name == "usama")
+System.out.println("its correct");
+if (name != "usama")
+System.out.println("its not correct");
+
+
+
+}
+
+
+
+
+
+
+}
