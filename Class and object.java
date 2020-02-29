fddb31254dab0792aaf543e26c49284e3e554298
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2020-03-01 00:03:04 +0500

    Add files via upload

---
+++ Class and object.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class BMI{
+String name;
+int id;
+
+
+
+public static void main(String[] args)
+
+{
+BMI s1=new BMI();
+s1.name="usama";
+s1.id=1;
+
+System.out.println("name is " + s1.name);
+System.out.println("id is " + s1.id);
+}
+}
