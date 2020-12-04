a0c931790687841efb72c6f8f0a530443cb1cf6f
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2020-12-04 20:37:27 +0500

    Create Sorting Stack.java

---
+++ Data Structures and Algorithms/Stack/Sorting Stack.java
@@ -0,0 +1,26 @@
+
+import java.util.Stack;
+public class Main {
+ public static  Stack<Integer> sortStack(Stack<Integer> input){
+         
+        Stack<Integer> tmpStack = new Stack<Integer>();
+           while(!input.isEmpty()) {
+            int tmp = input.pop();
+            while(!tmpStack.isEmpty() && tmpStack.peek() < tmp) {
+                input.push(tmpStack.pop());
+            }
+            tmpStack.push(tmp);   }
+
+      System.out.println("after sorting" + tmpStack);
+            
+       return tmpStack;   }
+    public static void main(String args[]){
+        Stack<Integer> input = new Stack<Integer>();
+        input.push(34);
+        input.push(3);
+        input.push(31);
+        input.push(98);
+        input.push(92);
+        input.push(23);
+        System.out.println("before sorting: "+input);   
+   sortStack(input);          }}
